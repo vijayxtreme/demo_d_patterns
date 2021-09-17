84ccaba4f2a02e93eea4ff0b36e06a9bb561b786
Vijay Menon <vijay@Vijays-MacBook-Pro.local>
2021-09-16 21:16:17 -0700

    Basic decorator example

--- src/snippets/adapter.js
+++ src/snippets/adapter.js
@@ -1,32 +1,41 @@
 import { concatAndOutput } from "../util";
 
-class Soldier {
-  constructor(level) {
-    this.level = level;
+class TealiumNew {
+  //the new api
+  pageView() {
+    console.log(`---page view the new way`);
   }
 
-  attack() {
-    return this.level * 1;
-  }
-}
-
-class Jedi {
-  constructor(level) {
-    this.level = level;
-  }
-
-  attackWithSaber() {
-    return this.level * 100;
+  pageClick() {
+    console.log(`---page click the new way`);
   }
 }
 
-class JediAdapter {
-  constructor(jedi) {
-    this.jedi = jedi;
+class TealiumLegacy {
+  //we still need to support the old way sometimes
+  view() {
+    console.log(`---page view the old way, but this method is deprecated`);
+  }
+  event() {
+    console.log(`---page click the old way, but this method is deprecated`);
+  }
+}
+
+class TealiumAdapter {
+  constructor(tealiumOld) {
+    this.tealiumOld = tealiumOld;
   }
 
-  attack() {
-    return this.jedi.attackWithSaber();
+  pageView() {
+    //our client will continue to call the new method per our interface
+    //but this adapter will help translate new to old for the time being
+    this.tealiumOld.view();
+  }
+
+  pageClick() {
+    //our client will continue to call the new method per our interface
+    //but this adapter will help translate new to old for the time being
+    return this.tealiumOld.event();
   }
 }
 const comments = `/* When an application may need to talk to 
@@ -34,6 +43,11 @@ another application (or different codebase), but an API is
 incompatible.  The adapter translates "requests" between one 
 application to another. */`;
 
-const output = concatAndOutput(comments, Soldier, Jedi, JediAdapter);
+const output = concatAndOutput(
+  comments,
+  TealiumNew,
+  TealiumLegacy,
+  TealiumAdapter
+);
 
 export default output;
